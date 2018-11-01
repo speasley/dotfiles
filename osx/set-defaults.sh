4fb35729e7b469100e7314011161e765e42404e5
Stephen Peasley <speasley@users.noreply.github.com>
2018-11-01 10:38:15 -0600

    Set CGFontRenderingFontSmoothingDisabled

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -39,3 +39,6 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Restore subpixel antialiasing for text in macOS Mojave
+defaults write -g CGFontRenderingFontSmoothingDisabled -bool FALSE
